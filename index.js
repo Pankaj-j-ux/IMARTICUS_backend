98daac9e1e163474c074a8f810847da357bef679
Pankaj Sharma <442001ps@gmail.com>
2023-04-02 00:32:20 +0530

    Update 2

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 /** @format */
 
 const app = require("./app");
-const connectWithDb = require("./config/db.js");
+// const connectWithDb = require("./config/db.js");
 require("dotenv").config();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 4000;
 
-// const mongoose = require("mongoose");
-// Connect with database
-connectWithDb();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at port : ${process.env.PORT}`);
-});
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected ...");
+    app.listen(PORT, () => {
+      console.log(`Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Database Connection Issue ...`);
+    console.log(error);
+    process.exit(1);
+  });
